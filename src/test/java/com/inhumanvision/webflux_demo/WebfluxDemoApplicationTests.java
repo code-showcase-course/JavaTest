60b9a513ccdf5827f75887556d8ac6ac341cab63
Luis Caamano <palagroup@gmail.com>
2024-08-18 22:05:17 -0400

    firsttt

---
+++ src/test/java/com/inhumanvision/webflux_demo/WebfluxDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inhumanvision.webflux_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebfluxDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
